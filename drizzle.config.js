a94a552ea71d1e97e1d30ae751f2ce3763816329
Yash Sonalekar <ysonalekar02@gmail.com>
2026-06-01 17:38:42 +0530

    restructured code

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export default defineConfig({
-  schema: "./schema",
-  out: "./migration",
+  schema: "./src/schema",
+  out: "./src/migration",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.POSTGRES_URL,
